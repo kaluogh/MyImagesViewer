071950aeb2b05c51c72b1898f5c4e8d30ec29616
kaluo <zhangwenxin6615006@163.com>
2019-11-15 09:41:22 +0800

    add files

---
+++ imageocrtranslate/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace imageocrtranslate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Window;
+            this.button1.Location = new System.Drawing.Point(213, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(61, 21);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.HideSelection = false;
+            this.treeView1.Location = new System.Drawing.Point(30, 87);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(244, 317);
+            this.treeView1.TabIndex = 2;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(20, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(623, 342);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(30, 432);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(302, 36);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1280, 720);
+            this.panel1.TabIndex = 5;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(199, 432);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(30, 482);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 7;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(159, 482);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(30, 523);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(244, 233);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(30, 781);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1604, 881);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ imageocrtranslate/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace imageocrtranslate
+{
+    public partial class Form1 : Form
+    {
+        //private string path;
+        //private string currentRootPath;
+        //private string currentFilePath;
+        //private TreeNode currentRootNode;
+        private TreeNode currentSelectNode;
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(textBox1.Text))
+            {
+                DirectoryInfo di = new DirectoryInfo(textBox1.Text);
+                TreeNode tn = new TreeNode(textBox1.Text);
+                tn.Tag = "Root";
+                treeView1.Nodes.Clear();
+                treeView1.Nodes.Add(tn);
+                currentSelectNode = tn;
+            }
+            else {
+                textBox1.Text = @"C:\Users\admin\Desktop\工作";
+                DirectoryInfo di = new DirectoryInfo(textBox1.Text);
+                TreeNode tn = new TreeNode(textBox1.Text);
+                tn.Tag = "Root";
+                treeView1.Nodes.Clear();
+                treeView1.Nodes.Add(tn);
+                currentSelectNode = tn;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            createDrivers();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+        }
+
+        private void createDrivers()
+        {
+            DirectoryInfo di;
+            TreeNode tn;
+            //string s;
+            foreach (string ss in Environment.GetLogicalDrives())
+            {
+                /**
+                 * ss的值形式如下：
+                 * C:\
+                 * D:\
+                 * E:\
+                 * F:\
+                 * I:\
+                 */
+                di = new DirectoryInfo(ss); //生成路径
+                if (di.Exists) //如果当前路径存在
+                {
+                    //s = ss.Substring(0, ss.IndexOf("\\")); //去掉路径的 \ 得到如：C:
+                    tn = new TreeNode(ss); //设置结点的值
+                    tn.Tag = "Root";
+                    treeView1.Nodes.Add(tn); //把结点加入到TreeView中
+                }
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            TreeNode tn = e.Node; //得到发生选择事件的结点
+            string currentPath = tn.FullPath;
+            currentSelectNode = tn;
+            //textBox1.Text = currentPath; //在左上角的文本框显示完整的路径
+            if (tn.Tag.ToString() == "File") //如果当前结点是文件
+            {
+                string ext = currentPath.Substring(currentPath.LastIndexOf(".") + 1); //得到文件的后缀
+                ext = ext.ToLower();
+                if (ext == "ico" || ext == "gif" || ext == "jpg" || ext == "png" || ext == "bmp") {
+                    //如果是图片，设置到右边的pictureBox
+                    try
+                    {
+                        pictureBox1.Image = Image.FromFile(currentPath);
+                        Size tempSize = pictureBox1.Image.Size;
+                        pictureBox1.Width = panel1.Width * 95 / 100;
+                        pictureBox1.Height = pictureBox1.Width * tempSize.Height / tempSize.Width;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else {
+                    //否则使用系统默认的工具打开该文件
+                    //System.Diagnostics.Process.Start(path);
+                }
+            }
+            else
+            {
+                //反之当前结点是文件夹
+                getSubContents(tn);//得到当前结点的全部内容
+            }
+        }
+
+        private void getSubContents(TreeNode parent)
+        {
+            DirectoryInfo di;
+            TreeNode tn;
+            di = new DirectoryInfo(parent.FullPath + "\\"); //得到路径
+            if (!di.Exists) return;             //路径不存在，直接返回
+            foreach (DirectoryInfo d in di.GetDirectories()) //该路径下面的所有 文件夹
+            {
+                tn = new TreeNode(d.Name);
+                tn.Tag = "Directory";
+                parent.Nodes.Add(tn); //将该节点添加到父节点
+            }
+            FileInfo[] fileList = di.GetFiles();
+            Array.Sort<FileInfo>(fileList, (s1, s2) => s1.Name.CompareTo(s2.Name));
+            foreach (FileInfo f in fileList)
+            {
+                
+                string temp = f.Extension;
+                if (!String.IsNullOrWhiteSpace(temp)) {
+                    if (temp == ".ico" || temp == ".gif" || temp == ".jpg" || temp == ".png" || temp == ".bmp")
+                    {
+                        tn = new TreeNode(f.Name); //该路径下的所有 文件
+                        tn.Tag = "File";
+                        parent.Nodes.Add(tn);
+                    }
+                }
+                
+            }
+        }
+
+        //prev button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (currentSelectNode.PrevNode != null) {
+                currentSelectNode = currentSelectNode.PrevNode;
+                treeView1.SelectedNode = currentSelectNode;
+                if (currentSelectNode.Tag.ToString() == "File") //如果当前结点是文件
+                {
+                    try
+                    {
+                        pictureBox1.Image = Image.FromFile(currentSelectNode.FullPath);
+                        Size tempSize = pictureBox1.Image.Size;
+                        pictureBox1.Width = panel1.Width * 95 / 100;
+                        pictureBox1.Height = pictureBox1.Width * tempSize.Height / tempSize.Width;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else //反之当前结点是文件夹
+                {
+                    getSubContents(currentSelectNode);//得到当前结点的全部内容
+                }
+
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (currentSelectNode.NextNode != null)
+            {
+                currentSelectNode = currentSelectNode.NextNode;
+                treeView1.SelectedNode = currentSelectNode;
+                if (currentSelectNode.Tag.ToString() == "File") //如果当前结点是文件
+                {
+                    try
+                    {
+                        pictureBox1.Image = Image.FromFile(currentSelectNode.FullPath);
+                        Size tempSize = pictureBox1.Image.Size;
+                        pictureBox1.Width = panel1.Width * 95 / 100;
+                        pictureBox1.Height = pictureBox1.Width * tempSize.Height / tempSize.Width;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else //反之当前结点是文件夹
+                {
+                    getSubContents(currentSelectNode);//得到当前结点的全部内容
+                }
+
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int tempWidth = int.Parse(textBox2.Text);
+                if (tempWidth > 600 && tempWidth < 1800) {
+                    panel1.Width = tempWidth;
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "abc\n123";
+        }
+    }
+}
